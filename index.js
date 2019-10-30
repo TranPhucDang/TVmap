840a93b6f412ec9ef54c26c3ab4d74b4e236dc47
TranPhucDang <pdang2812@gmail.com>
2019-10-30 17:39:55 +0700

    push map

--- index.js
+++ index.js
@@ -1,33 +1,56 @@
 import 'ol/ol.css';
 import Map from 'ol/Map';
 import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
-import TileWMS from 'ol/source/TileWMS';
+import ImageLayer from 'ol/layer/Image';
+import ImageWMS from 'ol/source/ImageWMS';
 
 
 
-
-var layers = [
-  new TileLayer({
-    source: new OSM()
-  }),
-  new TileLayer({
-  
-    source: new TileWMS({
-      url: 'http://localhost:8081/geoserver/vnm/wms',
-      params: {'LAYERS': 'vnm:vnm_adm1', 'TILED': true},
-      serverType: 'geoserver',
-      // Countries have transparency, so do not fade tiles:
-      transition: 0
-    })
-  })
-];
-var map = new Map({
-  layers: layers,
-  target: 'map',
-  view: new View({
-    center: [11816899.9708, 1580982.6893],
-    zoom: 4
-  })
+var wmsSource = new ImageWMS({
+  url: 'http://localhost:8080/geoserver/vnm/wms',
+  params: {'LAYERS': 'vnm:vnm_adm1'},
+  serverType: 'geoserver',
+  crossOrigin: 'anonymous'
 });
+
+var wmsLayer = new ImageLayer({
+  source: wmsSource
+});
+
+var view = new View({
+  center: [11816899.9708, 1580982.6893],
+  zoom: 6
+});
+
+var map = new Map({
+  layers: [wmsLayer],
+  target: 'map',
+  view: view
+});
+
+map.on('singleclick', function(evt) {
+  document.getElementById('info').innerHTML = '';
+  var viewResolution = /** @type {number} */ (view.getResolution());
+  var url = wmsSource.getFeatureInfoUrl(
+    evt.coordinate, viewResolution, 'EPSG:3857',
+    {'INFO_FORMAT': 'text/html'});
+  if (url) {
+    fetch(url)
+      .then(function (response) { return response.text(); })
+      .then(function (html) {
+        document.getElementById('info').innerHTML = html;
+      });
+  }
+});
+
+map.on('pointermove', function(evt) {
+  if (evt.dragging) {
+    return;
+  }
+  var pixel = map.getEventPixel(evt.originalEvent);
+  var hit = map.forEachLayerAtPixel(pixel, function() {
+    return true;
+  });
+  map.getTargetElement().style.cursor = hit ? 'pointer' : '';
+});
+
